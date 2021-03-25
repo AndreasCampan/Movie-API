940d2ce4cff53c748ceae63bec173f3ac62eee16
AndreasCampan <71665169+AndreasCampan@users.noreply.github.com>
2021-03-25 16:37:09 -0600

    Updated project based on task requirements
    -Added a css page for styling the documentation page
    -Added URL endpoints for each of the achievements objectives
    -Added content to the documentation page

--- index.js
+++ index.js
@@ -2,12 +2,24 @@ const express = require('express');
 const morgan = require('morgan');
 
 const app = express();
-const topMovies = [
+const movies = [
   {
     title: 'Forrest Gump',
+    genre: [
+      'drama',
+      'romance'
+    ],
+    director: 'Robert Zemeckis',
+    image: 'https://upload.wikimedia.org/wikipedia/en/6/67/Forrest_Gump_poster.jpg'
   },
   {
     title: 'Cast Away',
+    genre: [
+      'drama',
+      'adventure'
+    ],
+    director: 'Robert Zemeckis',
+    image: 'https://m.media-amazon.com/images/M/MV5BN2Y5ZTU4YjctMDRmMC00MTg4LWE1M2MtMjk4MzVmOTE4YjkzXkEyXkFqcGdeQXVyNTc1NTQxODI@._V1_UY1200_CR90,0,630,1200_AL_.jpg'
   },
   {
     title: 'The Imitation Game',
@@ -16,7 +28,7 @@ const topMovies = [
     title: 'Shawshank Redemption',
   },
   {
-    title: 'Saving Private Ryan',
+    title: 'The Martian',
   },
   {
     title: "A Knight's Tale",
@@ -42,16 +54,44 @@ app.use((err, req, res, next) => {
   res.status(500).send('The Planet hosting the server must have exploded!');
 });
 
-app.get('/movies', (req, res) => {
-  res.json(topMovies);
-});
-
-app.get('/', (req, res) => {
-  res.send('Welcome to my top 10 movies!');
-});
-
 app.get('/documentation', (req, res) => {
   res.sendFile('public/documentation.html', { root: __dirname });
+});
+
+app.get('/movies', (req, res) => {
+  res.json(movies);
+});
+
+app.get('/movies/:title', (req, res) => {
+  res.json(movies.find((movie) => movie.title === req.params.title));
+});
+
+app.get('/movies/genres/:name', (req, res) => {
+  res.send('Here is a description of the ________ movie genre!');
+});
+
+app.get('/directors/:name', (req, res) => {
+  res.send('Here is the data about the director!');
+});
+
+app.post('/users', (req, res) => {
+  res.send('Thank you for subscribing to myFlix!');
+});
+
+app.put('/users/:name/:nameChange', (req, res) => {
+  res.send('Thank you for updating your username!');
+});
+
+app.delete('/users/:name', (req, res) => {
+  res.send('Your account has been deleted!');
+});
+
+app.post('/users/:name/favMovies/:title', (req, res) => {
+  res.send('Your movie has been added to your favourites!');
+});
+
+app.delete('/users/:name/favMovies/:title', (req, res) => {
+  res.send('Your movie has been removed from your favourites!');
 });
 
 app.listen(8080, () => {
