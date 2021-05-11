917295aad551ce6cfb7abb62d8719b5a0acb6959
AndreasCampan <71665169+AndreasCampan@users.noreply.github.com>
2021-05-11 13:28:07 -0600

    Update index.js

--- index.js
+++ index.js
@@ -177,7 +177,7 @@ app.put('/users/:Username', passport.authenticate('jwt', { session: false }), [
     {
       $set: {
         Username: req.body.Username || preData.Username,
-        Password: console.log(Users.hashPassword(req.body.Password)) || preData.Password,
+        Password: req.body.Password || preData.Password,
         Email: req.body.Email || preData.Email,
         DOB: req.body.DOB || preData.DOB
       }
